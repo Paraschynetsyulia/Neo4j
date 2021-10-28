89ddc9fe7286ee98408a70e9d8f6ad4fc65511b9
Paraschynetsyulia <62383952+Paraschynetsyulia@users.noreply.github.com>
2021-10-28 22:03:50 +0300

    Delete urls.py

--- src/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path, include
-
-from .views import login_user, register, logout_user
-
-urlpatterns = [
-    path('login/', login_user, name='login'),
-    path('register/', register, name='register'),
-    path('logout/', logout_user, name='logout_user')
-]
